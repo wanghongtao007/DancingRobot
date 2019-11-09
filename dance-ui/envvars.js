759a2d2fe36a04faa7af94a9a146be819b2a3860
wanghongtao007 <wanhuatong@126.com>
2019-11-09 22:17:10 +0800

    Update envvars.js

--- dance-ui/envvars.js
+++ dance-ui/envvars.js
@@ -1,6 +1,5 @@
-var URL_TRACKING_DEFAULT = "http://dancingrobot-robot-ml.apps.cluster-anz-7e19.anz-7e19.open.redhat.com"
-var URL_DANCEAPI_DEFAULT = "http://dance-translate-robot-dance.apps.cluster-anz-7e19.anz-7e19.open.redhat.com"
+var URL_TRACKING_DEFAULT = "http://dancingrobot-robot.apps.cluster-685f.685f.sandbox380.opentlc.com"
+var URL_DANCEAPI_DEFAULT = "http://dance-translate-robot.apps.cluster-685f.685f.sandbox380.opentlc.com"
 //var URL_TRACKING = {{URL_TRACKING}};
 //var URL_DANCEAPI = {{URL_DANCEAPI}};
-
 
